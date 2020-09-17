a466cf6ba175cd2e1f596c2d1893a78cda3a2bd8
bill <ddzyan@163.com>
2020-09-17 18:31:29 +0800

    重复完善

--- algorithm/排序/选择/test.js
+++ algorithm/排序/选择/test.js
@@ -8,21 +8,20 @@ const arr = [1, 3, 7, 9, 2, 4, 6];
 function sort(arr) {
   const len = arr.length;
   let count = 0;
-  // 第一次循环表示已排序区间大小和当前插入的已排序位置
+  // 有序区间大小，循环变量为当前要插入的位置
   for (let i = 0; i < len; i++) {
     count++;
     let index = i;
-    // 第二次循环为了找出未排序区间最小值
+    // 找出最小元素
     for (let j = i + 1; j < len; j++) {
       count++;
       if (arr[j] < arr[index]) {
         index = j;
       }
     }
-
-    const tmp = arr[index];
-    arr[index] = arr[i];
-    arr[i] = tmp;
+    const tmp = arr[i];
+    arr[i] = arr[index];
+    arr[index] = tmp;
   }
   console.log('count', count);
   return arr;
