a88f5a8bf52645f84bec8c9eb85249069e6e1d1a
ddz <ddzyan@163.com>
2020-09-24 07:39:41 +0800

    添加快速排序代码

---
+++ algorithm/排序/快速/index.js
@@ -0,0 +1,48 @@
+/**
+ * 快速排序
+ * 核心思想：使用递归，直到长度小于2，对数组按照分割点进行切分，小的在左边，大的再右边
+ */
+
+/**
+ *
+ * @param {Array<number>} arr
+ * @param {number} start
+ * @param {number} end
+ */
+function sort(arr, start, end) {
+  if (start >= end) return;
+
+  const pivotPoint = partition(a, start, end);
+  sort(arr, start, pivotPoint - 1);
+  sort(arr, pivotPoint, end);
+}
+
+/**
+ * @description 将数组按照分割点进行站位，并且返回分割点
+ * @returns {Array<number>} arr
+ * @returns {number} start
+ * @returns {number} end
+ */
+function partition(arr, start, end) {
+  const pivotValue = arr[end - 1];
+  let i = start;
+
+  for (let j = start; j < end - 1; j++) {
+    if (arr[j] < pivotValue) {
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;
+    }
+  }
+
+  const temp = arr[i];
+  arr[i] = arr[end];
+  arr[end] = temp;
+  return i;
+}
+
+module.exports = function (arr) {
+  sort(arr, 0, arr.length - 1);
+  return arr;
+};
--- test/algorithm/排序/index.test.js
+++ test/algorithm/排序/index.test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 
 describe('排序测试', function () {
-  const arr = require('./data')(10000, 100);
+  const arr = require('./data')(100, 100);
   this.slow(1);
-  it('冒泡测试', () => {
+  it('冒泡', () => {
     const sort = require('../../../algorithm/排序/冒泡/test');
     const { arr: newArr, count } = sort(arr);
 
@@ -11,7 +11,7 @@ describe('排序测试', function () {
     assert.strictEqual(newArr.toString(), arr.toString(), '排序结果错误');
   });
 
-  it('插入测试', () => {
+  it('插入', () => {
     const sort = require('../../../algorithm/排序/插入/test');
     const { arr: newArr, count } = sort(arr);
 
@@ -19,7 +19,7 @@ describe('排序测试', function () {
     assert.strictEqual(newArr.toString(), arr.toString(), '排序结果错误');
   });
 
-  it('选择测试', () => {
+  it('选择', () => {
     const sort = require('../../../algorithm/排序/选择/test');
     const { arr: newArr, count } = sort(arr);
 
@@ -27,8 +27,16 @@ describe('排序测试', function () {
     assert.strictEqual(newArr.toString(), arr.toString(), '排序结果错误');
   });
 
-  it('归并测试', () => {
-    const mergeSort = require('../../../algorithm/排序/归并/text');
+  it('归并', () => {
+    const mergeSort = require('../../../algorithm/排序/归并/test');
+    const { arr: newArr } = mergeSort(arr);
+
+    //assert.ok(count > 0, '运算次数错误');
+    assert.strictEqual(newArr.toString(), arr.toString(), '排序结果错误');
+  });
+
+  it('快速', () => {
+    const mergeSort = require('../../../algorithm/排序/快速/test');
     const { arr: newArr } = mergeSort(arr);
 
     //assert.ok(count > 0, '运算次数错误');
