241c410905bced3ddbed6fd44d85bc7dda29dd01
bill <ddzyan@163.com>
2020-10-07 17:47:50 +0800

    优化二分查找单元测试

--- test/algorithm/二分查找/index.test.js
+++ test/algorithm/二分查找/index.test.js
@@ -1,11 +1,15 @@
 const assert = require('assert');
 const search = require('../../../algorithm/二分查找/binarySearch_01');
 
-describe('递归测试', () => {
-  it('在有序不重复的数组中，查找指定的元素', () => {
-    const arr = [1, 2, 3, 4, 5, 6, 10, 12, 13, 18, 20];
-    const res = search(arr, 10);
-    assert(res.count > 0, 'count 错误');
-    assert.strictEqual(res.index, 6, 'index 错误');
+describe('二分查找', () => {
+  describe('在有序不重复的数组中，查找指定的元素', () => {
+    it('test1', () => {
+      console.time('test1');
+      const arr = [-1, 0, 3, 5, 9, 12];
+      const res = search(arr, 9);
+      assert(res.count > 0, 'count 错误');
+      assert.strictEqual(res.index, 4, 'index 错误');
+      console.timeEnd('test1');
+    });
   });
 });
