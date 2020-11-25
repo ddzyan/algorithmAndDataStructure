136ddc48af234512d599a0a3c58bc880085b465a
ddz <ddzyan@163.com>
2020-11-25 10:09:14 +0800

    修改文件夹路径

---
+++ algorithm/字符串匹配/bf.js
@@ -0,0 +1,27 @@
+﻿/**
+ * @description 使用 BF 算法实现字符串匹配
+ * @param {string} string
+ */
+const indexOfA = function (str) {
+  const m = str.length;
+  const n = this.length;
+  if (m === 0) return -1;
+
+  const count = n - m + 1;
+  for (let i = 0; i <= count; i++) {
+    let strA = '';
+    for (let j = i; j < i + m; j++) {
+      strA += this[j];
+    }
+    if (str === strA) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+String.prototype.indexOfA = indexOfA;
+
+const a = 'hello word';
+console.log(a.indexOfA('aa'));
