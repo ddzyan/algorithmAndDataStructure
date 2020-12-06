44a61a7c70d0d9e35fb5f453976e7d4b6c71aadc
ddz <ddzyan@163.com>
2020-12-06 23:10:35 +0800

    添加leetCode q112买卖股票的最佳时机I解法

--- test/algorithm/贪心算法/index.test.js
+++ test/algorithm/贪心算法/index.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 const { mainSort, mainHashMap } = require('../../../algorithm/贪心算法/q242有效的字母异位词');
 const maxProfit = require('../../../algorithm/贪心算法/q122买卖股票的最佳时机II');
+const maxProfitOne = require('../../../algorithm/贪心算法/q121买卖股票的最佳时机');
 
 describe('贪心算法', function () {
   describe('q242有效的字母异位词', function () {
@@ -21,6 +22,13 @@ describe('贪心算法', function () {
       assert.strictEqual(maxProfit([7, 1, 5, 3, 6, 4]), 7, '结果返回错误');
       assert.strictEqual(maxProfit([1, 2, 3, 4, 5]), 4, '结果返回错误');
       assert.strictEqual(maxProfit([7, 6, 4, 3, 1]), 0, '结果返回错误');
+    });
+  });
+
+  describe('q112买卖股票的最佳时机I', function () {
+    it('test', function () {
+      assert.strictEqual(maxProfitOne([7, 1, 5, 3, 6, 4]), 5, '结果返回错误');
+      assert.strictEqual(maxProfitOne([7, 6, 4, 3, 1]), 0, '结果返回错误');
     });
   });
 });
