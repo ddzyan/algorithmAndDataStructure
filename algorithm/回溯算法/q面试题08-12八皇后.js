4f20ff4bc7ace75ee1de6db7830742326a270f10
ddz <ddzyan@163.com>
2020-12-08 00:06:37 +0800

    添加回溯算法说明，并且添加 q面试题08-12八皇后 题目

---
+++ algorithm/回溯算法/q面试题08-12八皇后.js
@@ -0,0 +1,26 @@
+/**
+ 设计一种算法，打印 N 皇后在 N × N 棋盘上的各种摆法，
+ 其中每个皇后都不同行、不同列，也不在对角线上。
+ 这里的“对角线”指的是所有的对角线，不只是平分整个棋盘的那两条对角线。
+*/
+
+/**
+ *
+ * @param {number} row 当前行数
+ */
+const cal8queens = function (row) {};
+
+/**
+ * @description 判断 row 行 column 列放置是否合适
+ * @param {number} row 行
+ * @param {number} column 列
+ * @returns {boolean}
+ */
+const isOk = function (row, column) {};
+
+/**
+ * @description 打印一个二位矩阵
+ * @param {Array} res
+ * @returns {void}
+ */
+const printQueens = function (res) {};
